6a1461826d1b767edd6f9c9b5712a516e3d5a90b
Renato <renato.f.fabri@gmail.com>
2024-01-21 14:30:51 -0300

    Start of pixel art generator

--- 01-PixelArtGenerator/index.js
+++ 01-PixelArtGenerator/index.js
@@ -1,1 +1,10 @@
- 
+const gridWidthInput = document.getElementById("gridWidth");
+const gridHeightInput = document.getElementById("gridHeight");
+
+const gridWidthLabel = document.getElementById("gridWidth-label");
+const gridHeightLabel = document.getElementById("gridHeight-label");
+
+const btnCreate = document.getElementById("optn-create");
+const btnClear = document.getElementById("optn-clear");
+const btnErase = document.getElementById("optn-erase");
+const btnPaint = document.getElementById("optn-paint");
