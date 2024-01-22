7671e5af024578746846a49f22cc37557b3361fe
Renato <renato.f.fabri@gmail.com>
2024-01-21 22:59:03 -0300

    add event listeners for grid

--- 01-PixelArtGenerator/index.js
+++ 01-PixelArtGenerator/index.js
@@ -8,15 +8,19 @@ const btnErase = document.getElementById("btn-erase");
 const btnPaint = document.getElementById("btn-paint");
 const colorPicker = document.getElementById("picker");
 const gridCanvas = document.querySelector(".gridCanvas");
+let gridTiles;
 
+//Update width Label
 gridWidthInput.addEventListener("change", () => {
   gridWidthLabel.innerText = gridWidthInput.value;
 });
 
+//Update height Label
 gridHeightInput.addEventListener("change", () => {
   gridHeightLabel.innerText = gridHeightInput.value;
 });
 
+//Create grid
 btnCreate.addEventListener("click", () => {
   const rows = gridHeightInput.value;
   const cols = gridWidthInput.value;
@@ -28,8 +32,34 @@ btnCreate.addEventListener("click", () => {
     for (let col = 0; col < cols; col++) {
       const gridTile = document.createElement("div");
       gridTile.className = "grid-tile";
+      gridTile.style.backgroundColor = "none";
       newRow.appendChild(gridTile);
     }
     gridCanvas.appendChild(newRow);
   }
+  gridTiles = document.querySelectorAll(".grid-tile");
+});
+
+//==============================================================
+
+// mousedown mousemove mouseup
+// touchstart touchmove touchend
+
+let isDrawing = false;
+let isErasing = false;
+
+gridCanvas.addEventListener("mouseover", () => {
+  gridTiles.forEach((tile) => {
+    tile.addEventListener("mousedown", () => {
+      isDrawing = true;
+      tile.style.backgroundColor = colorPicker.value;
+    });
+    tile.addEventListener("mouseup", () => {
+      isDrawing = false;
+    });
+
+    tile.addEventListener("mousemove", () => {
+      if (isDrawing) tile.style.backgroundColor = colorPicker.value;
+    });
+  });
 });
