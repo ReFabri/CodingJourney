ebcb97dd2877d4393114783a0310305cc1c653b0
Renato <renato.f.fabri@gmail.com>
2024-01-22 21:29:21 -0300

    Rich Text Editor start of OL, UL implementation

--- 02-RichTextEditor/index.js
+++ 02-RichTextEditor/index.js
@@ -2,6 +2,8 @@ const textEditor = document.querySelector(".text-editor");
 const btnBold = document.querySelector(".btn-bold");
 const btnSup = document.querySelector(".btn-sup");
 const btnSub = document.querySelector(".btn-sub");
+const btnOl = document.querySelector(".btn-ordered-list");
+const btnUl = document.querySelector(".btn-unordered-list");
 
 function getSelectedText() {
   let selection = window.getSelection();
@@ -31,5 +33,22 @@ btnSub.addEventListener("click", () => {
   if (!selection.isCollapsed) {
     let sub = document.createElement("sub");
     range.surroundContents(sub);
+  }
+});
+
+btnOl.addEventListener("click", () => {
+  const [selection, range] = getSelectedText();
+  console.log(range);
+  if (!selection.isCollapsed) {
+    let li = document.createElement("li");
+    range.surroundContents(li);
+  }
+});
+
+btnUl.addEventListener("click", () => {
+  const [selection, range] = getSelectedText();
+  if (!selection.isCollapsed) {
+    let ul = document.createElement("ul");
+    range.surroundContents(ul);
   }
 });
