bc4eac5df5601b9c86294126cf3bf1924d1cbc5c
Renato <renato.f.fabri@gmail.com>
2024-01-25 14:39:50 -0300

    Rich Text Editor add more event listeners

--- 02-RichTextEditor/index.js
+++ 02-RichTextEditor/index.js
@@ -4,6 +4,14 @@ const btnSup = document.querySelector(".btn-sup");
 const btnSub = document.querySelector(".btn-sub");
 const btnOl = document.querySelector(".btn-ordered-list");
 const btnUl = document.querySelector(".btn-unordered-list");
+const btnLink = document.querySelector(".btn-link");
+const btnAlignLeft = document.querySelector(".btn-align-left");
+const btnAlignCenter = document.querySelector(".btn-align-center");
+const btnAlignRight = document.querySelector(".btn-align-right");
+const btnAlignJustify = document.querySelector(".btn-align-justify");
+const btnHeadings = document.getElementById("headings");
+const btnFonts = document.getElementById("fonts");
+const btnFontSize = document.getElementById("fontSize");
 
 function getSelectedText() {
   let selection = window.getSelection();
@@ -30,6 +38,13 @@ btnBold.addEventListener("click", () => {
 btnSup.addEventListener("click", () => {
   const [selection, range] = getSelectedText();
 
+  if (
+    range.commonAncestorContainer.parentNode.classList.contains("text-editor")
+  ) {
+    let div = document.createElement("div");
+    range.surroundContents(div);
+  }
+
   let sup = document.createElement("sup");
   range.surroundContents(sup);
 });
@@ -37,21 +52,29 @@ btnSup.addEventListener("click", () => {
 btnSub.addEventListener("click", () => {
   const [selection, range] = getSelectedText();
 
+  if (
+    range.commonAncestorContainer.parentNode.classList.contains("text-editor")
+  ) {
+    let div = document.createElement("div");
+    range.surroundContents(div);
+  }
+
   let sub = document.createElement("sub");
   range.surroundContents(sub);
 });
 
-btnOl.addEventListener("click", () => {
-  const [selection, range] = getSelectedText();
-  console.log(range);
-
-  let li = document.createElement("li");
-  range.surroundContents(li);
-});
-
-btnUl.addEventListener("click", () => {
+btnLink.addEventListener("click", () => {
   const [selection, range] = getSelectedText();
 
-  let ul = document.createElement("ul");
-  range.surroundContents(ul);
+  if (
+    range.commonAncestorContainer.parentNode.classList.contains("text-editor")
+  ) {
+    let div = document.createElement("div");
+    range.surroundContents(div);
+  }
+
+  let link = document.createElement("a");
+  const url = window.prompt("Enter a Url:").toLowerCase();
+  link.setAttribute("href", url);
+  range.surroundContents(link);
 });
