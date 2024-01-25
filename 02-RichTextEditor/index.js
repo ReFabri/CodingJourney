99fe9651d0a0fd0787d12138bd5b5976e9767cb9
Renato <renato.f.fabri@gmail.com>
2024-01-24 21:23:07 -0300

    change to bold text implementation

--- 02-RichTextEditor/index.js
+++ 02-RichTextEditor/index.js
@@ -8,47 +8,50 @@ const btnUl = document.querySelector(".btn-unordered-list");
 function getSelectedText() {
   let selection = window.getSelection();
   let range = selection.getRangeAt(0);
-  return [selection, range];
+  if (!selection.isCollapsed) {
+    return [selection, range];
+  }
 }
 
 btnBold.addEventListener("click", () => {
   const [selection, range] = getSelectedText();
-  if (!selection.isCollapsed) {
-    let span = document.createElement("span");
-    span.style.fontWeight = "bold";
-    range.surroundContents(span);
+
+  if (
+    range.commonAncestorContainer.parentNode.classList.contains("text-editor")
+  ) {
+    let div = document.createElement("div");
+    div.classList.add("bold");
+    range.surroundContents(div);
+  } else {
+    range.commonAncestorContainer.parentNode.classList.toggle("bold");
   }
 });
 
 btnSup.addEventListener("click", () => {
   const [selection, range] = getSelectedText();
-  if (!selection.isCollapsed) {
-    let sup = document.createElement("sup");
-    range.surroundContents(sup);
-  }
+
+  let sup = document.createElement("sup");
+  range.surroundContents(sup);
 });
 
 btnSub.addEventListener("click", () => {
   const [selection, range] = getSelectedText();
-  if (!selection.isCollapsed) {
-    let sub = document.createElement("sub");
-    range.surroundContents(sub);
-  }
+
+  let sub = document.createElement("sub");
+  range.surroundContents(sub);
 });
 
 btnOl.addEventListener("click", () => {
   const [selection, range] = getSelectedText();
   console.log(range);
-  if (!selection.isCollapsed) {
-    let li = document.createElement("li");
-    range.surroundContents(li);
-  }
+
+  let li = document.createElement("li");
+  range.surroundContents(li);
 });
 
 btnUl.addEventListener("click", () => {
   const [selection, range] = getSelectedText();
-  if (!selection.isCollapsed) {
-    let ul = document.createElement("ul");
-    range.surroundContents(ul);
-  }
+
+  let ul = document.createElement("ul");
+  range.surroundContents(ul);
 });
