7c7d24ce7ee287115f9e21212b681b703c8315e0
Renato <renato.f.fabri@gmail.com>
2024-01-28 19:55:48 -0300

    BatteryApp add logic

--- 05-BatteryLevel/index.js
+++ 05-BatteryLevel/index.js
@@ -1,5 +1,22 @@
-const getBatteryInfo = async () => {
-  const batteryInfo = await navigator.getBattery();
-  console.log(batteryInfo);
+const currBatteryValue = document.querySelector(".battery-info h2");
+const chargeLabel = document.querySelector(".svg-wrapper h4");
+const chargeLevel = document.querySelector(".charge-level");
+const chargeIcon = document.querySelector(".charge-icon");
+
+const setDataToPage = async () => {
+  const { charging, level } = await navigator.getBattery();
+  currBatteryValue.innerText = `${level * 100}%`;
+  chargeLevel.style.height = `${level * 100}%`;
+  if (charging) {
+    chargeLabel.innerText = "Charging...";
+    chargeIcon.style.fill = "#94c411";
+  } else {
+    chargeLabel.innerText = "Discharging...";
+    chargeIcon.style.fill = "#dd0000";
+  }
 };
-getBatteryInfo();
+
+setDataToPage();
+setInterval(() => {
+  setDataToPage();
+}, 5000);
