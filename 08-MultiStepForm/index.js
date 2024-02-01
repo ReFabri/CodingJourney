4509fd8804471e29de42868455756ad0e381e17c
Renato <renato.f.fabri@gmail.com>
2024-02-01 15:48:35 -0300

    MultiStepForm initial styles and js

---
+++ 08-MultiStepForm/index.js
@@ -0,0 +1,24 @@
+const form = document.querySelector(".wrapper-form");
+const btnsNext = document.querySelectorAll(".btn-next");
+const btnsBack = document.querySelectorAll(".btn-back");
+const btnSubmit = document.querySelector(".btn-submit");
+
+// const formPos = window.getComputedStyle(form)
+// console.log(formPos.getPropertyValue("transform"));
+
+btnsNext.forEach((btnNext, i) => {
+  const posX = (i + 1) * 300 * -1;
+  btnNext.addEventListener("click", changePage(posX));
+});
+
+btnsBack.forEach((btnBack, i) => {
+  const posX = i * 300 * -1;
+  btnBack.addEventListener("click", changePage(posX));
+});
+
+function changePage(translateAmount) {
+  return () => {
+    form.style.transform = `translateX(${translateAmount}px)`;
+    console.log(translateAmount);
+  };
+}
