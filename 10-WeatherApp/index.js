64541c4bc5d38b372d53c2617670e539a56feba5
Renato <renato.f.fabri@gmail.com>
2024-02-04 17:41:52 -0300

    WeatherApp styles and js

--- 10-WeatherApp/index.js
+++ 10-WeatherApp/index.js
@@ -1,24 +1,32 @@
 const apiKey = prompt("Enter your api key:");
 
 const searchBtn = document.querySelector(".search-btn");
+const searchInput = document.querySelector(".search-wrapper input");
 const weatherImage = document.querySelector(".img-wrapper img");
 const temp = document.querySelector(".temperature-wrapper h2");
 const tempInfo = document.querySelector(".temperature-info h2");
 const [humidVal, windVal] = document.querySelectorAll(".moreInfo h3");
 
-const openWeatherAPI = `https://api.openweathermap.org/data/2.5/weather?q=london&units=metric&appid=${apiKey}`;
+searchInput.addEventListener("input", () => {
+  searchInput.value = searchInput.value.toUpperCase();
+});
 
-searchBtn.addEventListener("click", () => {
-  getWeatherData();
+searchBtn.addEventListener("click", async () => {
+  const weatherData = await getWeatherData();
+  changePageInfo(weatherData);
+  changeImage(weatherData.weather[0].main);
 });
 
 async function getWeatherData() {
   try {
-    const data = await fetch(openWeatherAPI);
-    const json = await data.json();
-    if (json) {
-      changePageInfo(json);
-      changeImage(json.weather[0].main);
+    const location = searchInput.value;
+    const openWeatherAPI = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${apiKey}`;
+    if (location) {
+      const data = await fetch(openWeatherAPI);
+      const json = await data.json();
+      if (json) {
+        return json;
+      }
     }
   } catch (error) {
     console.log(error);
