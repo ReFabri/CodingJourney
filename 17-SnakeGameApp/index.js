5642ee9b04269ffa3fe1296e4cf2b8ec339647df
Renato <renato.f.fabri@gmail.com>
2024-03-04 20:00:19 -0300

    SnakeGame js changes

--- 17-SnakeGameApp/index.js
+++ 17-SnakeGameApp/index.js
@@ -3,19 +3,34 @@ const tilesWrapper = document.querySelector(".tiles-wrapper");
 const [btnLeft, btnUp, btnRight, btnDown] =
   document.querySelectorAll(".bottom-bar button");
 
-const COLS = 30;
-const ROWS = 35;
+let snake = [{ x: 11, y: 11 }];
 
-function generateGrid() {
-  for (let i = 0; i < ROWS; i++) {
-    let row = document.createElement("div");
-    row.classList.add("row");
-    for (let j = 0; j < COLS; j++) {
-      let tile = document.createElement("div");
-      tile.classList.add("tile");
-      row.appendChild(tile);
-    }
-    tilesWrapper.appendChild(row);
-  }
+function drawGame() {
+  tilesWrapper.innerHTML = "";
+  drawSnake();
 }
-generateGrid();
+
+function createTile(className) {
+  const newTile = document.createElement("div");
+  newTile.classList.add(className);
+  return newTile;
+}
+
+function setTileCoords(tile, coords) {
+  tile.style.gridColumn = coords.x;
+  tile.style.gridRow = coords.y;
+}
+
+function drawSnake() {
+  snake.forEach((coord) => {
+    const snakeTile = createTile("snake-tile");
+    setTileCoords(snakeTile, coord);
+    tilesWrapper.appendChild(snakeTile);
+  });
+}
+
+function drawFood(coord) {
+  const foodTile = createTile("food-tile");
+  setTileCoords(foodTile, coord);
+  tilesWrapper.appendChild(foodTile);
+}
