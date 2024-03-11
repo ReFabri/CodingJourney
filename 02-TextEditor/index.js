57c9f3a07545a1bca65993bdd9da0ef283418c23
Renato <renato.f.fabri@gmail.com>
2024-03-10 23:09:46 -0300

    RichTextApp js rewrite and fixes

--- 02-TextEditor/index.js
+++ 02-TextEditor/index.js
@@ -1,214 +1,70 @@
-const textEditor = document.querySelector(".text-editor");
-const btnBold = document.querySelector(".btn-bold");
-const btnSup = document.querySelector(".btn-sup");
-const btnSub = document.querySelector(".btn-sub");
-const btnOl = document.querySelector(".btn-ordered-list");
-const btnUl = document.querySelector(".btn-unordered-list");
-const btnLink = document.querySelector(".btn-link");
-const btnAlignLeft = document.querySelector(".btn-align-left");
-const btnAlignCenter = document.querySelector(".btn-align-center");
-const btnAlignRight = document.querySelector(".btn-align-right");
-const btnAlignJustify = document.querySelector(".btn-align-justify");
-const btnHeadings = document.getElementById("headings");
-const btnFonts = document.getElementById("fonts");
-const btnFontSize = document.getElementById("fontSize");
-const btnFontColor = document.getElementById("fontColor");
-const btnFontHighlight = document.getElementById("highlightColor");
+const optionsButtons = document.querySelectorAll(".option-button");
+const advOptionsButtons = document.querySelectorAll(".adv-option-button");
+const fontSizeSelect = document.getElementById("fontSize");
+const fontFamilySelect = document.getElementById("fontName");
+const linkButton = document.getElementById("createLink");
 
-function getSelectedText() {
-  let selection = window.getSelection();
-  let range = selection.getRangeAt(0);
-  if (!selection.isCollapsed) {
-    return range;
-  }
+const alignButtons = document.querySelectorAll(".align");
+const spacingButtons = document.querySelectorAll(".spacing");
+const formatButtons = document.querySelectorAll(".format");
+const scriptButtons = document.querySelectorAll(".script");
+const writingArea = document.getElementById("text-input");
+
+console.log(advOptionsButtons);
+
+function initializer() {
+  highlighter(alignButtons, true);
+  highlighter(spacingButtons, true);
+  highlighter(formatButtons, false);
+  highlighter(scriptButtons, true);
+  fontSizeSelect.value = "12px";
 }
 
-btnBold.addEventListener("click", () => {
-  const range = getSelectedText();
+function highlighter(className, needsRemoval) {
+  className.forEach((button) => {
+    button.addEventListener("click", () => {
+      if (needsRemoval) {
+        let alreadyActive = false;
+        if (button.classList.contains("active")) {
+          alreadyActive = true;
+        }
+        className.forEach((button) => {
+          button.classList.remove("active");
+        });
+        if (!alreadyActive) {
+          button.classList.add("active");
+        }
+      } else {
+        button.classList.toggle("active");
+      }
+    });
+  });
+}
 
-  if (
-    range.commonAncestorContainer.parentNode.classList.contains("text-editor")
-  ) {
-    let div = document.createElement("div");
-    div.classList.add("bold");
-    range.surroundContents(div);
+const modifyText = (command, defaultUi, value) => {
+  document.execCommand(command, defaultUi, value);
+};
+
+optionsButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    modifyText(button.id, false, null);
+  });
+});
+
+advOptionsButtons.forEach((button) => {
+  button.addEventListener("change", () => {
+    modifyText(button.id, false, button.value);
+  });
+});
+
+linkButton.addEventListener("click", () => {
+  let userLink = prompt("Enter a URL?");
+  if (/http/i.test(userLink)) {
+    modifyText(linkButton.id, false, userLink);
   } else {
-    range.commonAncestorContainer.parentNode.classList.toggle("bold");
+    userLink = `http://${userLink}`;
+    modifyText(linkButton.id, false, userLink);
   }
 });
 
-btnSup.addEventListener("click", () => {
-  const range = getSelectedText();
-
-  if (
-    range.commonAncestorContainer.parentNode.classList.contains("text-editor")
-  ) {
-    let div = document.createElement("div");
-    range.surroundContents(div);
-  }
-
-  let sup = document.createElement("sup");
-  range.surroundContents(sup);
-});
-
-btnSub.addEventListener("click", () => {
-  const range = getSelectedText();
-
-  if (
-    range.commonAncestorContainer.parentNode.classList.contains("text-editor")
-  ) {
-    let div = document.createElement("div");
-    range.surroundContents(div);
-  }
-
-  let sub = document.createElement("sub");
-  range.surroundContents(sub);
-});
-
-btnLink.addEventListener("click", () => {
-  const range = getSelectedText();
-
-  if (
-    range.commonAncestorContainer.parentNode.classList.contains("text-editor")
-  ) {
-    let div = document.createElement("div");
-    range.surroundContents(div);
-  }
-
-  let link = document.createElement("a");
-  const url = window.prompt("Enter a Url:").toLowerCase();
-  link.setAttribute("href", url);
-  range.surroundContents(link);
-});
-
-btnAlignLeft.addEventListener("click", () => {
-  const range = getSelectedText();
-
-  if (
-    range.commonAncestorContainer.parentNode.classList.contains("text-editor")
-  ) {
-    let div = document.createElement("div");
-    range.surroundContents(div);
-  } else {
-    range.commonAncestorContainer.parentNode.style.textAlign = "left";
-  }
-});
-
-btnAlignCenter.addEventListener("click", () => {
-  const range = getSelectedText();
-
-  if (
-    range.commonAncestorContainer.parentNode.classList.contains("text-editor")
-  ) {
-    let div = document.createElement("div");
-    range.surroundContents(div);
-  } else {
-    range.commonAncestorContainer.parentNode.style.textAlign = "center";
-  }
-});
-
-btnAlignRight.addEventListener("click", () => {
-  const range = getSelectedText();
-
-  if (
-    range.commonAncestorContainer.parentNode.classList.contains("text-editor")
-  ) {
-    let div = document.createElement("div");
-    range.surroundContents(div);
-  } else {
-    range.commonAncestorContainer.parentNode.style.textAlign = "right";
-  }
-});
-
-btnAlignJustify.addEventListener("click", () => {
-  const range = getSelectedText();
-
-  if (
-    range.commonAncestorContainer.parentNode.classList.contains("text-editor")
-  ) {
-    let div = document.createElement("div");
-    range.surroundContents(div);
-  } else {
-    range.commonAncestorContainer.parentNode.style.textAlign = "justify";
-  }
-});
-
-btnHeadings.addEventListener("change", (e) => {
-  const range = getSelectedText();
-
-  if (
-    range.commonAncestorContainer.parentNode.classList.contains("text-editor")
-  ) {
-    let div = document.createElement("div");
-    range.surroundContents(div);
-  }
-  let heading = document.createElement(e.target.value);
-  range.surroundContents(heading);
-});
-
-btnFonts.addEventListener("change", (e) => {
-  const range = getSelectedText();
-
-  if (
-    range.commonAncestorContainer.parentNode.classList.contains("text-editor")
-  ) {
-    let div = document.createElement("div");
-    range.surroundContents(div);
-  } else {
-    range.commonAncestorContainer.parentNode.style.fontFamily = e.target.value;
-  }
-});
-
-btnFontSize.addEventListener("change", (e) => {
-  const range = getSelectedText();
-
-  if (
-    range.commonAncestorContainer.parentNode.classList.contains("text-editor")
-  ) {
-    let div = document.createElement("div");
-    range.surroundContents(div);
-  } else {
-    range.commonAncestorContainer.parentNode.style.fontSize = e.target.value;
-  }
-});
-
-btnFontColor.addEventListener("change", (e) => {
-  const range = getSelectedText();
-
-  if (
-    range.commonAncestorContainer.parentNode.classList.contains("text-editor")
-  ) {
-    let div = document.createElement("div");
-    range.surroundContents(div);
-  } else {
-    range.commonAncestorContainer.parentNode.style.color = e.target.value;
-  }
-});
-
-btnFontColor.addEventListener("change", (e) => {
-  const range = getSelectedText();
-
-  if (
-    range.commonAncestorContainer.parentNode.classList.contains("text-editor")
-  ) {
-    let div = document.createElement("div");
-    range.surroundContents(div);
-  } else {
-    range.commonAncestorContainer.parentNode.style.color = e.target.value;
-  }
-});
-
-btnFontHighlight.addEventListener("change", (e) => {
-  const range = getSelectedText();
-
-  if (
-    range.commonAncestorContainer.parentNode.classList.contains("text-editor")
-  ) {
-    let div = document.createElement("div");
-    range.surroundContents(div);
-  } else {
-    range.commonAncestorContainer.parentNode.style.display = "inline-block";
-    range.commonAncestorContainer.parentNode.style.backgroundColor =
-      e.target.value;
-  }
-});
+window.onload = initializer();
